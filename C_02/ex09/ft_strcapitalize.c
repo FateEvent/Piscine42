29af31e2709284d0b1a9e422510193485065f4b3
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-18 15:24:11 +0100

    capitalizedAchieved

--- C_02/ex09/ft_strcapitalize.c
+++ C_02/ex09/ft_strcapitalize.c
@@ -28,15 +28,12 @@ char	*ft_strcapitalize(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
 		}
-        if (str[i - 1] > 'Z' && str[i - 1] < 'a' || str[i - 1] > 'z')
+		if (str[i - 1] > 'Z' && str[i - 1] < 'a' || str[i - 1] > 'z')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
 		}
-		if (str[i - 1] >= '0' && str[i - 1] <= '9' || str[i - 1] >= 'A' && str[i - 1] <= 'Z')
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] += 32;
-        i++;
+		i++;
 	}
 	return (str);
 }
