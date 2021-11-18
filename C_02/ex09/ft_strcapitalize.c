e701d382e52aebf72dfdb0c997a018656b4ad0fd
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-18 15:10:56 +0100

    str_cap go for it!

--- C_02/ex09/ft_strcapitalize.c
+++ C_02/ex09/ft_strcapitalize.c
@@ -21,22 +21,22 @@ char	*ft_strcapitalize(char *str)
 		str[0] -= 32;
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= ' ') && (str[i] <= '@'))
+		if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+		if (str[i - 1] < '0' || str[i - 1] > '9' && str[i - 1] < 'A')
 		{
-			if ((str[i + 1] >= 'a') && (str[i + 1] <= 'z'))
-				str[i + 1] -= 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
 		}
-		if ((str[i] >= '[') && (str[i] <= '`'))
+        if (str[i - 1] > 'Z' && str[i - 1] < 'a' || str[i - 1] > 'z')
 		{
-			if ((str[i + 1] >= 'a') && (str[i + 1] <= 'z'))
-				str[i + 1] -= 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
 		}
-		if ((str[i] >= '{') && (str[i] <= '~'))
-		{
-			if ((str[i + 1] >= 'a') && (str[i + 1] <= 'z'))
-				str[i + 1] -= 32;
-		}
-		i++;
+		if (str[i - 1] >= '0' && str[i - 1] <= '9' || str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+        i++;
 	}
 	return (str);
 }
