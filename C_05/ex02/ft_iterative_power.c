741cf8c467940754f87b30feb2f7f925ff4b0a9f
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-20 14:41:15 +0100

    iterative_factorial_strikes

--- C_05/ex02/ft_iterative_power.c
+++ C_05/ex02/ft_iterative_power.c
@@ -6,25 +6,27 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 14:06:19 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/20 14:11:36 by faventur         ###   ########.fr       */
+/*   Updated: 2021/11/20 14:40:41 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb);
+int	ft_iterative_power(int nb, int power);
 
-int	ft_iterative_factorial(int nb)
+int	ft_iterative_power(int nb, int power)
 {
 	int	i;
 
 	i = nb;
-	if (nb < 0)
+	if (power < 0)
 		return (0);
-	if (nb == 0 || nb == 1)
+	if (power == 0)
 		return (1);
-	while (i > 1)
+	if (power == 1)
+		return (nb);
+	while (power > 1)
 	{
-		nb = nb * (i - 1);
-		i--;
+		nb = nb * i;
+		power--;
 	}
 	return (nb);
 }
