bf11cfdfe3919e46c9b14c7a57eeaa38b36f215b
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-20 14:04:13 +0100

    recursive mode on

--- C_05/ex01/ft_recursive_factorial.c
+++ C_05/ex01/ft_recursive_factorial.c
@@ -6,19 +6,19 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 14:56:53 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/19 15:10:57 by faventur         ###   ########.fr       */
+/*   Updated: 2021/11/20 14:01:03 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_recursive_factorial(int nb);
+int	ft_recursive_factorial(int nb);
 
-int ft_recursive_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-    if (nb < 0)
-        return (0);
-    if (nb == 0 || nb == 1)
-        return (1);
-    if (nb > 1)
-        return nb * ft_recursive_factorial(nb - 1);
-    return (0);
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	if (nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	return (0);
 }
--- C_05/ex03/ft_recursive_power.c
+++ C_05/ex03/ft_recursive_power.c
@@ -6,19 +6,21 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 15:19:45 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/19 15:25:15 by faventur         ###   ########.fr       */
+/*   Updated: 2021/11/20 14:03:00 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_recursive_power(int nb, int power);
+int	ft_recursive_power(int nb, int power);
 
-int ft_recursive_power(int nb, int power)
+int	ft_recursive_power(int nb, int power)
 {
-    if (nb < 0)
-        return (0);
-    if (nb == 0 || nb == 1)
-        return (1);
-    if (nb > 1)
-        return nb * ft_recursive_power(power - 1);
-    return (0);
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, power - 1));
+	return (0);
 }
