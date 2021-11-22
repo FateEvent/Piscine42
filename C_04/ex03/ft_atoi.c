a564e9f9299adcdfb4e88ef2272d31da4865cbb0
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-22 12:16:33 +0100

    atoi, prime and so what

--- C_04/ex03/ft_atoi.c
+++ C_04/ex03/ft_atoi.c
@@ -6,26 +6,33 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 21:45:13 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/19 13:54:53 by faventur         ###   ########.fr       */
+/*   Updated: 2021/11/22 12:13:56 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isspace(int _c);
+int ft_abs(int nb);
 int	ft_atoi(char *str);
 
 int	ft_atoi(char *str)
 {
 	int	i;
 	int	res;
+	int minus_count;
 
 	i = 0;
 	res = 0;
-	while (str[i] != 0)
+	minus_count = 0;
+	while (str[i] < 'a' || str[i] > 'z')
 	{
+		if (ft_isspace(str[i]) == 0 && str[i] == '-')
+			minus_count++;
 		if (ft_isspace(str[i]) == 0 && str[i] >= '0' && str[i] <= '9')
 			res = res * 10 + str[i] - '0';
-	++i;
+		++i;
 	}
+	if (minus_count % 2 == 1)
+			res = ft_abs(res);
 	return (res);
 }
 
@@ -35,4 +42,11 @@ int	ft_isspace(int _c)
 		return (1);
 	else
 		return (0);
+}
+
+int ft_abs(int nb)
+{
+    if (nb < 0)
+        return (nb * -1);
+    return (nb);
 }
--- C_05/ex06/ft_is_prime.c
+++ C_05/ex06/ft_is_prime.c
@@ -6,22 +6,24 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 19:06:53 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/21 19:17:10 by faventur         ###   ########.fr       */
+/*   Updated: 2021/11/22 12:04:42 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_is_prime(int nb);
 
-int	ft_is_prime(int nb)
+int ft_is_prime(int nb)
 {
-	int	i;
-
-	i = 0;
-	while (i < nb)
-	{
-		if ((i * i) == nb)
-			return (0);
-		i++;
-	}
-	return (1);
+    int i;
+    
+    i = 2;
+	if (nb <= 1)
+		return (0);
+    while (i < nb)
+    {
+        if (nb % i == 0)
+            return (0);
+        i++;
+    }
+    return (1);
 }
