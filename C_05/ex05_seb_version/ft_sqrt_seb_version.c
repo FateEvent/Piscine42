0057c168858bc83d71a06b1a8201e08d7d9b206c
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-26 11:21:05 +0100

    de racines carrées et de défaites aidant à grandir

--- C_05/ex05_seb_version/ft_sqrt_seb_version.c
+++ C_05/ex05_seb_version/ft_sqrt_seb_version.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_sqrt_seb_version.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/21 11:26:06 by faventur          #+#    #+#             */
-/*   Updated: 2021/11/26 11:02:44 by faventur         ###   ########.fr       */
+/*   Created: 2021/11/26 11:02:05 by faventur          #+#    #+#             */
+/*   Updated: 2021/11/26 11:02:07 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ int	ft_sqrt(int nb)
 		return (0);
 	else if (nb == 1)
 		return (1);
-	while (i < nb && i < 46341)
+	while (1)
 	{
-		if ((i * i) == nb)
+		if (i * i > nb || i == 46341)
+			return (0);
+		else if (i * i == nb)
 			return (i);
-		i++;
+		else
+			i++;
 	}
 	return (0);
 }
