f2cbb1bd1cb24c13b825cf60ba7c814ab38fb56c
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-26 14:26:55 +0100

    another version of a Makefile

---
+++ C_09/ex01_var/srcs/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 18:59:21 by faventur          #+#    #+#             */
+/*   Updated: 2021/11/14 18:59:27 by faventur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
