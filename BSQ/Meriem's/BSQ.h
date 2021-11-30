2758344d7a75b2098357aff0e4427ff6e7bf5391
FateEvent <fabio.venturini.1989@gmail.com>
2021-11-30 17:46:57 +0100

    le 9 est bon

--- BSQ/Meriem's/BSQ.h
+++ BSQ/Meriem's/BSQ.h
@@ -8,12 +8,18 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
-void        ft_putchar(char c);
-void        ft_putstr(char *str);
-int         ft_get_number_lines(char *argv);
-int         ft_size_file(char *argv);
-int         ft_get_number_lines(char *argv);
-int         ft_get_number_columns(char *argv);
-void        ft_get_second_line(int fd);
-char      **ft_read_file(char *argv);
+typedef	struct s_coordinates
+{
+	int	x;
+	int	y;
+}		t_coord;
+
+
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+int			ft_get_number_lines(char **argv);
+int			ft_size_file(char *argv);
+int			ft_get_number_columns(char **argv);
+void		ft_get_second_line(int fd);
+char		**ft_read_file(char *argv);
 #endif
--- BSQ/utils.c
+++ BSQ/utils.c
@@ -1,31 +1,15 @@
-int ft_row_counter(char *buf)
-{
-	int i;
-	int counter;
-
-	i = 0;
-	counter = 0;
-	while (buf[i] != '\0')
-	{
-		if (buf[i] == '\n')
-			counter++;
-		i++;
-	}
-	return (counter);
-}
+#include <stdio.h>
 
 /* Je retourne counter - 1 parce que j'elimine la premiere ligne mais + 1 pour la derniere ? */
 
-int ft_col_counter(char *buf)
+int ft_get_number_columns(char **argv)
 {
 	int i;
 	int counter;
 
 	i = 0;
 	counter = 0;
-	while (buf[i] && (buf[i] != '\n'))
-		i++;
-	while (buf[i] && (buf[i] != '\n'))
+	while (argv[1][i] && (argv[1][i] != '\n'))
 	{
 			counter++;
 			i++;
@@ -33,4 +17,34 @@ int ft_col_counter(char *buf)
 	return (counter);
 }
 
+int ft_get_number_lines(char **argv)
+{
+	int	i;
+	int	j;
+	int counter;
+
+	i = 1;
+	j = 0;
+	counter = 0;
+	while (argv[i][j] != '\0')
+	{
+		if (argv[i][j] == '\n')
+		{
+			counter++;
+			i++;
+			j = 0;
+		}
+		j++;
+	}
+	return (counter + 1);
+}
+
 /* C'est quand qu'on free ? */
+
+int	main()
+{
+	char	*str[] = {"4 .ox\n", "aaaaaaa\n", "aaaaaaa\n", "aaaaaaa\n", "aaaaaaa\0"};
+	int	c = ft_get_number_columns(str);
+	int	r = ft_get_number_lines(str);
+	printf("%d, \n%d", r, c);
+}
