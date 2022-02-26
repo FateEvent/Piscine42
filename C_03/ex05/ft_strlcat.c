86093120c41636c22cc77e2c427a402ab03557cd
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-26 18:04:05 +0100

    strlcat_parfait

--- C_03/ex05/ft_strlcat.c
+++ C_03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:23:48 by faventur          #+#    #+#             */
-/*   Updated: 2022/02/24 16:54:09 by faventur         ###   ########.fr       */
+/*   Updated: 2022/02/26 18:03:41 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,15 @@ int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
+	unsigned int	dest_len;
+	unsigned int	src_len;
 
 	i = 0;
 	j = ft_strlen(dest);
-	if (size < 0)
-		return (0);
+	dest_len = j;
+	src_len = ft_strlen(src);
+	if (size <= 0)
+		return (src_len);
 	while ((j < size - 1 && src[i] != '\0') && size != 0)
 	{
 		dest[j] = src[i];
@@ -29,7 +33,10 @@ int	ft_strlcat(char *dest, char *src, unsigned int size)
 		j++;
 	}
 	dest[j] = '\0';
-	return (ft_strlen(dest));
+	if (size < dest_len)
+		return (src_len + size);
+	else
+		return (src_len + dest_len);
 }
 
 unsigned int	ft_strlen(char *str)
