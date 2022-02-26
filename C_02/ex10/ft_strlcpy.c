527d833ebfd66e8c14430387194b5bc6da633c0d
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-26 15:47:11 +0100

    strlcpy

--- C_02/ex10/ft_strlcpy.c
+++ C_02/ex10/ft_strlcpy.c
@@ -10,20 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * The strlcpy() function copies a string. It is designed to be a safer, more consistent, and less
- * error prone replacement for strncpy(3). Unlike that function, strlcpy() takes the full size of the
- * buffer (not just the length) and guarantees to NUL-terminate the result (as long as size is larger
- * than 0. Note that a byte for the NUL should be included in size. Also note that strlcpy() only
- * operates on true "C" strings. This means that for src must be NUL-terminated.
- * 
- * The strlcpy() function copies up to size - 1 characters from the NUL-terminated string src to dst,
- * NUL-terminating the result.
- * 
- * Return Values: The strlcpy() function returns the total length of the string it tried to create.
- * It was done to make truncation detection simple.
- */
-
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
