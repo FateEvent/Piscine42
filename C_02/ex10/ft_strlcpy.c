be1eb487f693549886e18b94ea159d7885e5dfcd
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-26 17:21:42 +0100

    strlcpy_to_go

--- C_02/ex10/ft_strlcpy.c
+++ C_02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 21:03:03 by faventur          #+#    #+#             */
-/*   Updated: 2022/01/24 11:47:13 by faventur         ###   ########.fr       */
+/*   Updated: 2022/02/26 16:39:06 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	srclen = 0;
 	while (src[srclen])
 		srclen++;
-	if (size == 0 || srclen == '\0')
-		return (0);
+	if (size == 0)
+		return (srclen);
 	while (i < size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (i);
+	return (srclen);
 }
