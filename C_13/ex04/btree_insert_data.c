1a6f21975dc0d12a152157f1c14525058cbdeb54
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-03 15:03:20 +0200

    mission complete

--- C_13/ex04/btree_insert_data.c
+++ C_13/ex04/btree_insert_data.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 18:10:57 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/03 00:03:37 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/03 15:03:01 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	btree_insert_data(t_btree **root, void *item, int (*cmpf)
 	{
 		if (*root)
 		{
-			if (cmpf((*root)->item, item) < 0)
+			if (cmpf(item, (*root)->item) < 0)
 			{
 				if ((*root)->left)
 					btree_insert_data(&(*root)->left, item, cmpf);
