421b19125ad0d0305ffaa0b81edd40b8c5ce6cfa
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-04 14:14:41 +0200

    yo

--- C_13/ex05/ft_btree.h
+++ C_13/ex05/ft_btree.h
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 15:05:14 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/04 11:56:30 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/04 11:56:33 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,5 @@ void	btree_insert_data(t_btree **root, void *item, int (*cmpf)
 			(void *, void *));
 void	*btree_search_item(t_btree *root, void *data_ref, int (*cmpf)
 			(void *, void *));
-int		btree_level_count(t_btree *root);
 
 #endif
