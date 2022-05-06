385a77af68e652ddf8efa6570f94aac98e464bd7
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-07 00:09:21 +0200

    implementing a bubble sort

---
+++ C_12/ex14/ft_list_sort.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_sort.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 16:50:17 by faventur          #+#    #+#             */
+/*   Updated: 2022/05/07 00:09:09 by faventur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_merge(t_list **begin_list1, t_list *begin_list2)
+{
+	t_list	*current;
+
+	current = *begin_list1;
+	while (current->next)
+		current = current->next;
+	current->next = begin_list2;
+}
+
+https://www.geeksforgeeks.org/bubble-sort/
