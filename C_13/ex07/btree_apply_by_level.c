be4cf05cda39503be5762bc7234122747b726529
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-06 13:16:49 +0200

    I can find the level I ddesire

--- C_13/ex07/btree_apply_by_level.c
+++ C_13/ex07/btree_apply_by_level.c
@@ -6,37 +6,21 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 11:23:18 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/06 12:46:41 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/06 13:16:24 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_btree.h"
-
+#include <stdio.h>
+/*
 void	btree_apply_by_level(t_btree *root, void (*applyf)
 			(void *item, int current_level, int is_first_elem))
 {
 	
 }
-
-int	btree_level_count(t_btree *root)
-{
-	int	lheight;
-	int	rheight;
-
-	if (root)
-	{
-		lheight = btree_level_count(root->left);
-		rheight = btree_level_count(root->right);
-		if (lheight > rheight)
-			return (lheight + 1);
-		else
-			return (rheight + 1);
-	}
-	return (0);
-}
-
+*/
 // Function to print all nodes of a given level from left to right
-int	printLevel(t_btree *root, int level, int is_first_elem)
+int	print_level(t_btree *root, int level)
 {
 	int	left;
 	int	right;
@@ -47,24 +31,57 @@ int	printLevel(t_btree *root, int level, int is_first_elem)
 	}
 	if (level == 1)
 	{
-		applyf(root->item, level, is_first_elem);
+		printf("%d\n", *(int *)root->item);
 		// return true if at least one node is present at a given level
 		return 1;
 	}
-	left = printLevel(root->left, level - 1, is_first_elem);
-	right = printLevel(root->right, level - 1, is_first_elem);
+	left = print_level(root->left, level - 1);
+	right = print_level(root->right, level - 1);
 	return (left || right);
 }
 // Function to print level order traversal of a given binary tree
-void	levelOrderTraversal(t_btree *root, int is_first_elem)
+void	levelOrderTraversal(t_btree *root)
 {
 	// start from level 1 — till the height of the tree
 	int level;
 
-	level = 1;
-	// run till printLevel() returns false
-	while (printLevel(root, level, is_first_elem))
-		level++;
+	level = 4;
+	// run till print_level() returns false
+	print_level(root, level);
 }
 
-https://www.techiedelight.com/level-order-traversal-binary-tree/
+int	cmp(void *s1, void *s2)
+{
+	if (*(int *)s1 != *(int *)s2)
+		return (*(int *)s1 - *(int *)s2);
+	return (0);
+}
+
+void	ft(void *nb)
+{
+	*(int *)nb *= 5;
+//	printf("%d\n", *(int *)nb);
+}
+
+int main()
+{
+	t_btree	*tree;
+	int	a = 1;
+	int	b = 5;
+	int	c = 3;
+	int	d = 15;
+	int	e = 10;
+	int f = 6;
+	int g = 0;
+	int l = -1;
+
+	tree = NULL;
+	btree_insert_data(&tree, &a, cmp);
+	btree_insert_data(&tree, &b, cmp);
+	btree_insert_data(&tree, &c, cmp);
+	btree_insert_data(&tree, &d, cmp);
+	btree_insert_data(&tree, &f, cmp);
+	btree_insert_data(&tree, &g, cmp);
+	btree_insert_data(&tree, &l, cmp);
+	levelOrderTraversal(tree);
+}
