8661ca57ab4787a0875fe273df74579aab9d58c5
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-06 23:25:23 +0200

    merging done

--- C_12/ex13/ft_list_merge.c
+++ C_12/ex13/ft_list_merge.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*   ft_list_merge.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:50:17 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/06 22:28:55 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/06 23:17:41 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref,
-			int (*cmp)(), void (*free_fct)(void *))
+void	ft_list_merge(t_list **begin_list1, t_list *begin_list2)
 {
 	t_list	*current;
-	t_list	*tmp;
 
-	current = begin_list;
-	while (current)
-	{
-		tmp = current->next;
-		if (!cmp(data_ref, current->data))
-		{
-			free_fct(current->data);
-			free(current);
-		}
-		current = tmp;
-	}
+	current = *begin_list1;
+	while (current->next)
+		current = current->next;
+	current->next = begin_list2;
 }
