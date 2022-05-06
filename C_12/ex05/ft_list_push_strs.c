a5b5934c9498d378b83af1bec20c185364bdb898
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-06 17:46:00 +0200

    .

--- C_12/ex05/ft_list_push_strs.c
+++ C_12/ex05/ft_list_push_strs.c
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_push_BACK.c                                :+:      :+:    :+:   */
+/*   ft_list_push_strs.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:50:17 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/06 17:35:41 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/06 17:45:51 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_push_back(t_list **begin_list, void *data)
+t_list	*ft_list_push_strs(int size, char **strs)
 {
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		
+	}
 	t_list	*new;
 
 	new = ft_create_elem(data);
